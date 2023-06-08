add57498a9ac7dae652f5d0a162b8c5484266147
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-08 12:52:39 +0200

    add new, filters and completed

---
+++ src/components/ToDos.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const todos = ref([{
+  name: 'Une toute nouvelle tâche',
+  completed: false
+}])
+
+let newTodo = ''
+let filter = ref('all')
+
+const remaining = computed(() => todos.value.filter(todo => !todo.completed).length)
+
+function addTodo() {
+  todos.value.push({
+    completed: false,
+    name: newTodo
+  })
+  newTodo = ''
+}
+
+const filteredTodos = computed(() => {
+  if (filter.value === 'todo') {
+    return todos.value.filter(todo => !todo.completed)
+  } else if (filter.value === 'done') {
+    return todos.value.filter(todo => todo.completed)
+  }
+
+  return todos.value
+})
+
+</script>
+
+<template>
+  <section class="todoapp">
+    <header class="header">
+
+      <h1>Todos</h1>
+
+      <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+    </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in  filteredTodos " :key="todo.id" :class="{ completed: todo.completed }">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle">
+            <label>{{ todo.name }}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <footer class="footer">
+      <span class="todo-count">Reste à faire <strong>{{ remaining }}</strong> tâches.</span>
+      <ul class="filters">
+        <li> <a href="#" :class="{ selected: filter === 'all' }" @click.prevent="filter = 'all'">Toutes</a>
+        </li>
+        <li> <a href="#" :class="{ selected: filter === 'todo' }" @click.prevent="filter = 'todo'">A faire</a>
+        </li>
+        <li> <a href="#" :class="{ selected: filter === 'done' }" @click.prevent="filter = 'done'">Faites</a>
+        </li>
+      </ul>
+
+    </footer>
+  </section>
+</template>
+
+<styles src="./ToDos.css"></styles>
